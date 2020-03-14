3aebc5e43bae2f7134ef5781a0049e2b1ec28f9f
davide <Pre55T0day>
2020-03-14 17:36:53 +0100

    Aggiornamento docs

--- build-docs.sh
+++ build-docs.sh
@@ -5,10 +5,6 @@ set -e
 npm run build
 echo 'vue-ui-table docs buildato'
 
-rm -rf docs/*
-cp -r dist/ docs/
-echo 'vue-ui-table docs aggiornata'
-
 git add -A
 git commit -m 'Aggiornamento docs'
 git push
